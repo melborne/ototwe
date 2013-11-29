01efb69cecf3bcc964620da4f6d2eb8aab74cd36
kyoendo <postagie@gmail.com>
2013-11-29 10:03:32 +0900

    Refactoring

--- lib/ototwe/tweet_stream.rb
+++ lib/ototwe/tweet_stream.rb
@@ -6,11 +6,10 @@ class TweetStream
   URL = 'https://stream.twitter.com/1/statuses/filter.json'
 
   attr_accessor :track_terms
-  def initialize(track_terms:[], consumer_key: nil, consumer_secret: nil, access_token: nil, access_token_secret: nil)
+  def initialize(track_terms:[], auth:{})
     @track_terms = track_terms
-    auth = {consumer_key: consumer_key, consumer_secret: consumer_secret,
-            access_token: access_token, access_token_secret: access_token_secret}
-    validate_auth(auth)
+    @conn = EM::HttpRequest.new(URL, inactivity_timeout: 0)
+    @conn.use EM::Middleware::OAuth, validate_auth(auth)
   end
   
   def ontweet(&blk)
@@ -18,9 +17,7 @@ class TweetStream
   end
 
   def listen
-    conn = EM::HttpRequest.new(URL, inactivity_timeout: 0)
     http = conn.post(track: @track_terms.join(','))
-    
     parser.on_parse_complete = @callback
     http.stream { |data| parser << data if data }
   rescue => e
@@ -29,8 +26,9 @@ class TweetStream
 
   private
   def validate_auth(auth)
-    unless auth.any? { |k,v| v.nil? }
-      conn.use EM::Middleware::OAuth, auth
+    keys = %i(consumer_key consumer_secret access_token access_token_secret)
+    if (keys - auth.keys).empty? && auth.all? { |_,val| !val.nil? }
+      auth
     else
       raise "Must pass oauth authenticate code"
     end
